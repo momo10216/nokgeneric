de0710f8d3f399fefa903eba3d82227f6ca5f0d6
Norbert Kuemin <momo_102@bluemail.ch>
2016-09-10 13:21:15 +0200

    Fix pagination

--- html/pagination.php
+++ html/pagination.php
@@ -72,10 +72,13 @@ function pagination_list_footer($list)
 {
 	$html = '<div class="pagination">'."\n";
 	if (isset($list['pageslinks'])) { $html .= $list['pageslinks']."\n"; }
-	if (isset($list['prefix'])) { $prefix = $list['prefix']; } else { $prefix = ''; }
-	if (isset($list['limitstart'])) { $limitstart = $list['limitstart']; } else { $limitstart = ''; }
+	$prefix = '';
+	if (isset($list['prefix'])) { $prefix = $list['prefix']; }
+	$limitstart = '';
+	if (isset($list['limitstart'])) { $limitstart = $list['limitstart']; }
 	$html .= '<input type="hidden" name="'.$prefix.'limitstart" value="'.$limitstart.'" />'."\n";
-	$html .= '</div>';
+	$html .= '</div>'."\n";
+
 	return $html;
 }
 
@@ -96,7 +99,7 @@ function pagination_list_render($list)
 	$step = 5;
 	if (isset($list['pages'])) {
 		foreach ($list['pages'] as $k => $page) {
-			if (!isset($list['active'] || (!$page['active'])) {
+			if (!isset($list['active']) || !$page['active']) {
 				$currentPage = $k;
 			}
 		}
@@ -110,18 +113,33 @@ function pagination_list_render($list)
 	}
 
 	$html = '<ul class="pagination-list">';
-	$html .= $list['start']['data'];
-	$html .= $list['previous']['data'];
-	foreach ($list['pages'] as $k => $page) {
-		if (in_array($k, range($range * $step - ($step + 1), $range * $step))) {
-			if (($k % $step == 0 || $k == $range * $step - ($step + 1)) && $k != $currentPage && $k != $range * $step - $step) {
-				$page['data'] = preg_replace('#(<a.*?>).*?(</a>)#', '$1...$2', $page['data']);
+	if (isset($list['start']) && isset($list['start']['data'])) {
+		$html .= $list['start']['data'];
+	}
+	if (isset($list['previous']) && isset($list['previous']['data'])) {
+		$html .= $list['previous']['data'];
+	}
+
+	if (isset($list['pages'])) {
+		foreach ($list['pages'] as $k => $page) {
+			if (in_array($k, range($range * $step - ($step + 1), $range * $step))) {
+				if (($k % $step == 0 || $k == $range * $step - ($step + 1)) && $k != $currentPage && $k != $range * $step - $step) {
+					$page['data'] = preg_replace('#(<a.*?>).*?(</a>)#', '$1...$2', $page['data']);
+				}
+			}
+			if (isset($page['data'])) {
+				$html .= $page['data'];
 			}
 		}
-		$html .= $page['data'];
 	}
-	$html .= $list['next']['data'];
-	$html .= $list['end']['data'];
+
+	if (isset($list['next']) && isset($list['next']['data'])) {
+		$html .= $list['next']['data'];
+	}
+	if (isset($list['end']) && isset($list['end']['data'])) {
+		$html .= $list['end']['data'];
+	}
+
 	$html .= '</ul>';
 	return $html;
 }
@@ -135,29 +153,41 @@ function pagination_list_render($list)
  *
  * @since   3.0
  */
-function pagination_item_active(&$item) {
+function pagination_item_active(&$item)
+{
 	$class = '';
+
 	// Check for "Start" item
-	if ($item->text == JText::_('JLIB_HTML_START')) {
+	if ($item->text == JText::_('JLIB_HTML_START'))
+	{
 		$display = '<i class="icon-first"></i>';
 	}
+
 	// Check for "Prev" item
-	if ($item->text == JText::_('JPREV')) {
+	if ($item->text == JText::_('JPREV'))
+	{
 		$display = '<i class="icon-previous"></i>';
 	}
+
 	// Check for "Next" item
-	if ($item->text == JText::_('JNEXT')) {
+	if ($item->text == JText::_('JNEXT'))
+	{
 		$display = '<i class="icon-next"></i>';
 	}
+
 	// Check for "End" item
-	if ($item->text == JText::_('JLIB_HTML_END')) {
+	if ($item->text == JText::_('JLIB_HTML_END'))
+	{
 		$display = '<i class="icon-last"></i>';
 	}
+
 	// If the display object isn't set already, just render the item with its text
-	if (!isset($display)) {
+	if (!isset($display))
+	{
 		$display = $item->text;
 		$class   = ' class="hidden-phone"';
 	}
+
 	return '<li' . $class . '><a title="' . $item->text . '" href="' . $item->link . '" class="pagenav">' . $display . '</a></li>';
 }
 
@@ -170,27 +200,38 @@ function pagination_item_active(&$item) {
  *
  * @since   3.0
  */
-function pagination_item_inactive(&$item) {
+function pagination_item_inactive(&$item)
+{
 	// Check for "Start" item
-	if ($item->text == JText::_('JLIB_HTML_START')) {
+	if ($item->text == JText::_('JLIB_HTML_START'))
+	{
 		return '<li class="disabled"><a><i class="icon-first"></i></a></li>';
 	}
+
 	// Check for "Prev" item
-	if ($item->text == JText::_('JPREV')) {
+	if ($item->text == JText::_('JPREV'))
+	{
 		return '<li class="disabled"><a><i class="icon-previous"></i></a></li>';
 	}
+
 	// Check for "Next" item
-	if ($item->text == JText::_('JNEXT')) {
+	if ($item->text == JText::_('JNEXT'))
+	{
 		return '<li class="disabled"><a><i class="icon-next"></i></a></li>';
 	}
+
 	// Check for "End" item
-	if ($item->text == JText::_('JLIB_HTML_END')) {
+	if ($item->text == JText::_('JLIB_HTML_END'))
+	{
 		return '<li class="disabled"><a><i class="icon-last"></i></a></li>';
 	}
+
 	// Check if the item is the active page
-	if (isset($item->active) && ($item->active)) {
+	if (isset($item->active) && ($item->active))
+	{
 		return '<li class="active hidden-phone"><a>' . $item->text . '</a></li>';
 	}
+
 	// Doesn't match any other condition, render a normal item
 	return '<li class="disabled hidden-phone"><a>' . $item->text . '</a></li>';
 }
