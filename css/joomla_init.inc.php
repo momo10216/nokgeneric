c89cf1d04603a2273c3513f69fdd9991e2091bbc
Norbert Kuemin <momo_102@bluemail.ch>
2017-02-03 09:17:29 +0100

    More fail safe

--- css/joomla_init.inc.php
+++ css/joomla_init.inc.php
@@ -144,8 +144,8 @@ if (isset($_GET['styleId'])) {
 		$params[$varname] = $templateParams->get($varname);
 	}
 }
-if (empty($params['templateFontSize'])) { $params['templateFontSize'] = '12'; }
-if (empty($params['templateParagraphMarginTop'])) { $params['templateParagraphMarginTop'] = '0'; }
-if (empty($params['templateParagraphMarginBottom'])) { $params['templateParagraphMarginBottom'] = '1'; }
-if (empty($params['menuFontSize'])) { $params['menuFontSize'] = '12'; }
+if (!iiset($params['templateFontSize']) || empty($params['templateFontSize'])) { $params['templateFontSize'] = '12'; }
+if (!iiset($params['templateParagraphMarginTop']) || empty($params['templateParagraphMarginTop'])) { $params['templateParagraphMarginTop'] = '0'; }
+if (!iiset($params['templateParagraphMarginBottom']) || empty($params['templateParagraphMarginBottom'])) { $params['templateParagraphMarginBottom'] = '1'; }
+if (!iiset($params['menuFontSize']) || empty($params['menuFontSize'])) { $params['menuFontSize'] = '12'; }
 ?>
